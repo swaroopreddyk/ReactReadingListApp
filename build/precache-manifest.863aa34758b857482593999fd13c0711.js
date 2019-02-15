088088695aea42a2a9003cca525df75cea653fec
Swaroop Kondreddy <swaroopreddy.k@gmail.com>
2019-02-14 21:13:36 -0500

    init build

---
+++ build/precache-manifest.863aa34758b857482593999fd13c0711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55fc0043e462291c4f05",
+    "url": "/static/js/main.55fc0043.chunk.js"
+  },
+  {
+    "revision": "5ef16d925075505b55c9",
+    "url": "/static/js/2.5ef16d92.chunk.js"
+  },
+  {
+    "revision": "55fc0043e462291c4f05",
+    "url": "/static/css/main.87d28abd.chunk.css"
+  },
+  {
+    "revision": "638f4e61385203fb6c38a3e4d70ab161",
+    "url": "/index.html"
+  }
+];
